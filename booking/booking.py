497b3b790f920c1891e7b531201d46fad0bfa7d5
MehdiB <elmehdibaghdadi123@gmail.com>
2025-09-26 14:43:14 +0100

    ajout movie, user et booking

--- booking/booking.py
+++ booking/booking.py
@@ -16,6 +16,57 @@ def home():
    return "<h1 style='color:blue'>Welcome to the Booking service!</h1>"
 
 
+# GET all movies
+@app.route("/movies", methods=["GET"])
+def get_movies():
+    return jsonify(movies)
+
+# GET one movie by id
+@app.route("/movies/<movie_id>", methods=["GET"])
+def get_movie(movie_id):
+    for m in movies:
+        if m["id"] == movie_id:
+            return jsonify(m)
+    return make_response(jsonify({"error": "Movie not found"}), 404)
+
+@app.route("/movies/<movieid>", methods=['POST'])
+def add_movie(movieid):
+    req = request.get_json()
+
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            print(movie["id"])
+            print(movieid)
+            return make_response(jsonify({"error":"movie ID already exists"}),500)
+
+    movies.append(req)
+    write(movies)
+    res = make_response(jsonify({"message":"movie added"}),200)
+    return res
+
+@app.route("/movies/<movieid>/<rate>/<title>", methods=['PUT'])
+def update_movie_rating(movieid, rate , title):
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            movie["rating"] = rate
+            movie["title"] = title
+            res = make_response(jsonify(movie),200)
+            write(movies)
+            return res
+
+    res = make_response(jsonify({"error":"movie ID not found"}),500)
+    return res
+
+@app.route("/movies/<movieid>", methods=['DELETE'])
+def del_movie(movieid):
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            movies.remove(movie)
+            return make_response(jsonify(movie),200)
+
+    res = make_response(jsonify({"error":"movie ID not found"}),500)
+    return res
+
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
    app.run(host=HOST, port=PORT)
--- user/user.py
+++ user/user.py
@@ -15,6 +15,58 @@ with open('{}/databases/users.json'.format("."), "r") as jsf:
 def home():
    return "<h1 style='color:blue'>Welcome to the User service!</h1>"
 
+def write(users):
+    with open('{}/databases/users.json'.format("."), 'w') as f:
+        full = {}
+        full['users'] = users
+        json.dump(full, f)
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    return jsonify(users)
+
+@app.route("/users/<user_id>", methods=["GET"])
+def get_user_byid(user_id):
+    for u in users :
+        if u["id"] == user_id :
+            return jsonify(u) 
+    return make_response(jsonify({"error": "user not found"}), 404)
+
+@app.route("/users/<userid>", methods=['POST'])
+def add_movie(userid):
+    req = request.get_json()
+
+    for u in users:
+        if str(u['id']) == str(userid):
+            print(u['id'])
+            print(userid)
+            return make_response(jsonify({"error" : "user ID already exists"}),500)
+
+    users.append(req)
+    write(users)
+    return make_response(jsonify({"message":"user added"}),200)
+   
+@app.route("/users/<userid>/<lastactive>",methods=["PUT"])
+def user_update(userid,lastactive):
+    for u in users : 
+        if str(u["id"]) == str(userid):
+            u["last_active"] = lastactive
+            res = make_response(jsonify({"user updated"}),200)
+            write(users)
+            return res
+        
+    return make_response(jsonify({"user not found"}),500)
+
+@app.route("/users/<userid>", methods=["DELETE"]) 
+def user_delete(userid):
+    for u in users : 
+        if str(u["id"]) == userid :
+            users.remove(u)
+            write(users)
+            return make_response(jsonify(u),200)
+        
+    return make_response(jsonify({"error":"user not found"}),500)
+
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
