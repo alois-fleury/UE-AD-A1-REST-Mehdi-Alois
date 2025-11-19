73021c6b2a1fefb79b41b3226d1251218153e4f0
Aloïs Fleury <fleuryalois@gmail.com>
2025-11-19 22:30:49 +0100

    corrections

--- checkAdmin.py
+++ checkAdmin.py
@@ -1,10 +1,12 @@
 import requests
 
-USER_SERVICE_URL = "http://localhost:3203/"
+USER_SERVICE_URL = "http://127.0.0.1:3203/"
 
 def checkAdmin(user_id):
+    print("on passe par checkadmin")
     try:
         resp = requests.get(f"{USER_SERVICE_URL}/users/isadmin/{user_id}?uid={user_id}")
+        print("on a eu le get")
         if resp.status_code == 200:
             return resp.json().get("admin", False)
     except Exception:
